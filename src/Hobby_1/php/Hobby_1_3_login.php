ffe15e147456c2ba9183c405d89ada8a15a45e0d
haveAbook0or7 <haveabook.w@gmail.com>
2021-11-26 10:33:09 +0900

    一応伏せ字に変更。

--- src/Hobby_1/php/Hobby_1_3_login.php
+++ src/Hobby_1/php/Hobby_1_3_login.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <title>ツイステカード編成ツール</title>
+    <title>twstカード編成ツール</title>
         <meta charset="UTF-8"><meta name = "authr" content="有本 和奏">
         <link href="../css/H_common.css" rel="stylesheet">
         <script src="../lib/vue.js"></script> 
@@ -11,7 +11,7 @@
     </head>
     <body>
         <header>
-            <h1>ツイステカード編成ツール<a href="./Hobby_1_1_home.php" id="homelink">ホームへ戻る</a></h1>
+            <h1>twstカード編成ツール<a href="./Hobby_1_1_home.php" id="homelink">ホームへ戻る</a></h1>
         </header>
         <main id="app">
             <br><br><br><br>
